6b5f4592c866927b2376cf1933bab51703218a76
Archangel354 <rduncan354@gmail.com>
2017-07-28 16:53:37 -0400

    Adding updated MainActivity.java

--- app/src/main/java/com/example/owner/habittracker/MainActivity.java
+++ app/src/main/java/com/example/owner/habittracker/MainActivity.java
@@ -1,13 +1,112 @@
 package com.example.owner.habittracker;
 
-import android.support.v7.app.AppCompatActivity;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.example.owner.habittracker.data.HabitContract;
+import com.example.owner.habittracker.data.HabitDbHelper;
 
 public class MainActivity extends AppCompatActivity {
+
+    /** Database helper that will provide us access to the database */
+    private HabitDbHelper mDbHelper;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDbHelper = new HabitDbHelper(this);
+        displayDatabaseInfo();
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        displayDatabaseInfo();
+    }
+
+    /**
+     * Temporary helper method to display information in the onscreen TextView about the state of
+     * the pets database.
+     */
+    private void displayDatabaseInfo() {
+        // Create and/or open a database to read from it
+        SQLiteDatabase db = mDbHelper.getReadableDatabase();
+
+        // Define a projection that specifies which columns from the database
+        // you will actually use after this query.
+        String[] projection = {
+                HabitContract.HabitEntry._ID,
+                HabitContract.HabitEntry.COLUMN_HABIT_NAME,
+                HabitContract.HabitEntry.COLUMN_COMMENT,
+                HabitContract.HabitEntry.COLUMN_STARTTIME.toString(),
+                HabitContract.HabitEntry.COLUMN_ENDTIME.toString(),
+                HabitContract.HabitEntry.COLUMN_DURATION.toString()};
+
+        // Perform a query on the pets table
+        Cursor cursor = db.query(
+                HabitContract.HabitEntry.TABLE_NAME,   // The table to query
+                projection,            // The columns to return
+                null,                  // The columns for the WHERE clause
+                null,                  // The values for the WHERE clause
+                null,                  // Don't group the rows
+                null,                  // Don't filter by row groups
+                null);                   // The sort order
+
+        TextView displayView = (TextView) findViewById(R.id.text_view_habit);
+
+        try {
+            // Create a header in the Text View that looks like this:
+            //
+            // The pets table contains <number of rows in Cursor> pets.
+            // _id - name - breed - gender - weight
+            //
+            // In the while loop below, iterate through the rows of the cursor and display
+            // the information from each column in this order.
+            displayView.setText("The pets table contains " + cursor.getCount() + " pets.\n\n");
+            displayView.append(HabitContract.HabitEntry._ID + " - " +
+                    HabitContract.HabitEntry.COLUMN_HABIT_NAME + " - " +
+                    HabitContract.HabitEntry.COLUMN_COMMENT + " - " +
+                    HabitContract.HabitEntry.COLUMN_STARTTIME + " - " +
+                    HabitContract.HabitEntry.COLUMN_ENDTIME + " - " +
+                    HabitContract.HabitEntry.COLUMN_DURATION + "\n");
+
+            // Figure out the index of each column
+            int idColumnIndex = cursor.getColumnIndex(HabitContract.HabitEntry._ID);
+            int nameColumnIndex = cursor.getColumnIndex(HabitContract.HabitEntry.COLUMN_HABIT_NAME);
+            int commentColumnIndex = cursor.getColumnIndex(HabitContract.HabitEntry.COLUMN_COMMENT);
+            int startTimeColumnIndex = cursor.getColumnIndex(HabitContract.HabitEntry.COLUMN_STARTTIME.toString());
+            int  endTimeColumnIndex = cursor.getColumnIndex(HabitContract.HabitEntry.COLUMN_ENDTIME.toString());
+            int  durationColumnIndex = cursor.getColumnIndex(HabitContract.HabitEntry.COLUMN_DURATION.toString());
+
+            // Iterate through all the returned rows in the cursor
+            while (cursor.moveToNext()) {
+                // Use that index to extract the String or Int value of the word
+                // at the current row the cursor is on.
+                int currentID = cursor.getInt(idColumnIndex);
+                String currentName = cursor.getString(nameColumnIndex);
+                String currentComment = cursor.getString(commentColumnIndex);
+                int currentStartTime = cursor.getInt(startTimeColumnIndex);
+                int currentEndTime = cursor.getInt(endTimeColumnIndex);
+                int currentDuration = cursor.getInt(durationColumnIndex);
+                // Display the values from each column of the current row in the cursor in the TextView
+                displayView.append(("\n" + currentID + " - " +
+                        currentName + " - " +
+                        currentComment + " - " +
+                        currentStartTime + " - " +
+                        currentEndTime + " - " +
+                        currentDuration));
+            }
+        } finally {
+            // Always close the cursor when you're done reading from it. This releases all its
+            // resources and makes it invalid.
+            cursor.close();
+        }
     }
 }
